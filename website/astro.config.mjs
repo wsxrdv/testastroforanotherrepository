27a97e896a74eeb78f56bfcfaeb4dea37783e686
Karel Kral <karel@star-lab.xyz>
2024-09-16 10:39:40 +0000

    test

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wsxrdv.github.io',
-	base: '',
+	base: '/testastroforanotherrepository',
 	integrations: [
 		starlight({
 			title: 'My Docs',

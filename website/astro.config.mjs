a1798def83c475e90c035267575d4308a788aa32
Karel Kral <karel@star-lab.xyz>
2024-09-16 09:35:52 +0000

    test

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,4 +6,25 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	site: 'https://wsxrdv.github.io/',
 	base: '/testastroforanotherrepository',
+	integrations: [
+		starlight({
+			title: 'Test of Astro',
+			social: {
+				github: 'https://github.com/wsxrdv/testastroforanotherrepository',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/capture' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
